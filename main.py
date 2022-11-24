48b21610e5b1e2e07b5ece159fd25214e985c552
Jacob-h-v <jvillu21@student.aau.dk>
2022-11-24 13:16:49 +0100

    progress so far grassfire

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from Grassfire import grassfire
 inputImage = cv.imread("Resources/CroppedAndPerspectiveCorrected/1.jpg")
 template = cv.imread("Resources/CrownTemplate.jpg")
 
-# crop to play field if needed
 
 # 2) identify tiles and check for crowns on each tile
 identifiedTiles, identifiedCrowns = IdentifyTiles(inputImage)
@@ -17,38 +16,11 @@ print(identifiedTiles)
 print(identifiedCrowns)
 
 
-# 3) Get point-giving stuff
+# 3) Get points by checking for connected tiles and multiplying by number of crowns on each set of tiles
 scoreCount = grassfire(identifiedTiles, identifiedCrowns)
 print(scoreCount)
 
-# split image to H S and V channels. Might be useful for checking hue / value channels when matching areas
-hsvImage = SplitHSV(inputImage)
-H = hsvImage[:, :, 0]
-S = hsvImage[:, :, 1]
-V = hsvImage[:, :, 2]
-
-# Split play field into tiles
-
-
-
-# check for connected areas and count them
-
-# Multiply connected area score by number of crowns (bounding boxes) in area
-
-# 4) disregard the rest
-
 # 5) display outcome
 
-# cv.imshow("HSV Image", hsvImage)
-# cv.imshow("H", H)
-# cv.imshow("S", S)
-# cv.imshow("V", V)
-# cv.imshow("OutputFinal", outputFinal)
-# cv.imshow("template", template)
-# cv.imshow("template90", template90)
-# cv.imshow("template180", template180)
-# cv.imshow("template270", template270)
-# cv.imshow("Output1", output1)
-# cv.imshow("Output2", output2)
-# cv.imshow("Output3", output3)
+
 # cv.waitKey(0)
